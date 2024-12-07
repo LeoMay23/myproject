42ecc68f68e8ab2124d44f13316b573bef8a33f5
yikai lin <1958497468@qq.com>
2024-12-07 12:37:18 +0800

    已购买功能修正

--- purchased/serializers.py
+++ purchased/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from .models import PurchasedItem
+from product.serializers import ProductSerializer
 
 class PurchasedItemSerializer(serializers.ModelSerializer):
+    product = ProductSerializer()
     class Meta:
         model = PurchasedItem
         fields = ['id', 'user', 'product', 'quantity', 'purchase_date']
