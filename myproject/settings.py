016eccacf9e4a49dfa0f20ed1662658376bd38b4
yikai lin <1958497468@qq.com>
2024-12-08 15:57:07 +0800

    更换数据库

--- myproject/settings.py
+++ myproject/settings.py
@@ -88,12 +88,8 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'myshop',
-        'USER': 'root',
-        'PASSWORD': '050106',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
